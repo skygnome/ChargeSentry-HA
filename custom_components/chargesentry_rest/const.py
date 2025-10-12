0614a778acde698d2d6877a733511139e7e60ee4
skygnome <aaron@teksquad.co.uk>
2025-10-12 18:59:36 +0100

    Create const.py

---
+++ custom_components/chargesentry_rest/const.py
@@ -0,0 +1,6 @@
+DOMAIN = "chargesentry_rest"
+DEFAULT_SCAN_INTERVAL = 5  # seconds
+CONF_BASE_URL = "base_url"
+CONF_SERIAL = "serial"
+CONF_TOKEN = "token"  # optional bearer
+PLATFORMS = ["sensor"]
