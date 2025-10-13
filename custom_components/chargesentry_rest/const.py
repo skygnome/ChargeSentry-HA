1c9b9cd9bca377fa5843bf47654fea9431d3f1b8
skygnome <aaron@teksquad.co.uk>
2025-10-13 11:48:45 +0100

    Update const.py

--- custom_components/chargesentry_rest/const.py
+++ custom_components/chargesentry_rest/const.py
@@ -3,11 +3,9 @@ DOMAIN = "chargesentry_rest"
 # Hard-code polling to 120s
 DEFAULT_SCAN_INTERVAL = 120  # seconds
 
-# Config keys
 CONF_SERIAL = "serial"
 CONF_TOKEN = "token"
 
-# Hard-coded API endpoints
 ENERGY_URL = "https://api.chargesentry.uk/v1/live/energy/{serial}"
 LIVE_URL   = "https://api.chargesentry.uk/v1/live/details/{serial}"
 
